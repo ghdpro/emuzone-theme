85064c5a41bf4fd03beb305eb5c3eec416cf20e2
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2021-10-28 08:40:34 +0200

    Finish implementation of Share widget

--- classes/class-sharewidget.php
+++ classes/class-sharewidget.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Share widget
+ *
+ * Shows social share buttons for Facebook, Twitter & Reddit
+ */
+
 class ShareWidget extends WP_Widget {
 
 	public function __construct() {
@@ -10,9 +16,33 @@ class ShareWidget extends WP_Widget {
 		);
 	}
 
+	/**
+	 * Displays wiget output
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
 	public function widget( $args, $instance ) {
+        // Get the link for the current page, regardless of what kind of page it is
+        // So can't use get_permalink() here, because that doesn't work on category pages
+        // $GET is for capturing search page query string
 		global $wp;
-		$permalink = home_url(add_query_arg(array($_GET), $wp->request . '/'));
-		echo $permalink;
+		$permalink = home_url(add_query_arg(array( $_GET ), $wp->request . '/'));
+        // Get the title of the page, which is post/page title or blog title if not a singular post/page
+		if ( is_singular() )
+			$title = get_the_title();
+		else
+			$title = get_bloginfo();
+        // Escape for inclusion in query string
+		$permalink = rawurlencode( esc_url( $permalink ) );
+		$title = rawurlencode( $title );
+		?>
+        <h2 class="widget-title">Share</h2>
+			<p class="social">
+				<a class="facebook" href="https://www.facebook.com/sharer.php?u=<?php echo $permalink; ?>" target="_blank" title="Post on Facebook"><i class="fa fa-facebook"></i></a>
+				<a class="twitter" href="https://twitter.com/intent/tweet?url=<?php echo $permalink; ?>&amp;text=<?php echo $title; ?>" target="_blank" title="Post on Twitter"><i class="fa fa-twitter"></i></a>
+				<a class="reddit" href="https://reddit.com/submit?url=<?php echo $permalink; ?>&amp;title=<?php echo $title; ?>" target="_blank" title="Post on Reddit"><i class="fa fa-reddit-alien"></i></a>
+			</p>
+		<?php
 	}
 }
